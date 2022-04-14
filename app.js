c6a284a7761635179cdff68075a3de5dceb72ec3
Avetris <aitorvt95@gmail.com>
2022-04-14 17:25:14 +0200

    Test changing image folder path

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use('/data', express.static('../blog_images'));
+app.use('/data', express.static('./blog_images'));
 app.use(express.static(path.join(__dirname, 'build')));
 app.use('/api', admin);
 
